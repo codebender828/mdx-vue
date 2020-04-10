40bb374cb3a58666cebb838a81ceb0d3482b1788
codebender828 <jbakebwa@163.com>
2020-04-11 02:03:45 +0800

    feat(create-element): remove context components from attrs

--- src/create-element.js
+++ src/create-element.js
@@ -17,25 +17,31 @@ export default function(type, props, children) {
   const h = this.createElement
   const components = this.components
   const defaults = Object.keys(DEFAULTS)
-
-  // We check to see if props is of type object. If it is, then we pass them into the MDXContext component
-  const _props = typeof props === 'object' ? props : undefined
   
   let tag
+  let elProps = props
 
   // We check context to see if the element/tag
   // is provided in the MDXProvider context.
   if (Object.keys(components).includes(type)) {
-    tag = components[type](_props)
+    // We check to see if props is of type object.
+    // If it is, then we pass them into the MDXContext component
+    const componentProps = typeof props === 'object' ? props : undefined
+    tag = components[type](componentProps)
 
-    // Check to see target is included in defaults
   } else if (defaults.includes(type)) {
-    tag = DEFAULTS[type]
 
-    // Render final tag if component is not provided in context
+    tag = DEFAULTS[type]
+    // Remove components object from attrs
+    const { components, ...attrs } = elProps.attrs
+    elProps = {
+      attrs
+    }
+
+  // Render final tag if component is not provided in context
   } else {
     tag = type
   }
 
-  return h(tag, props, children)
+  return h(tag, elProps, children)
 }
